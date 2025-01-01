85b5ce4f22c9d459184d494eddbeec634202e4eb
Paulo Cantergiani <paulo13hcc@gmail.com>
2025-01-01 19:22:39 -0300

    update

--- STM32F411RE_ADC-Single-Conversion/src/adc.c
+++ STM32F411RE_ADC-Single-Conversion/src/adc.c
@@ -4,9 +4,9 @@
 #define APB1_EN (1 << 8)
 #define ADC_CH1 (1 << 0)
 #define ADC_SEQ_LEN_1 0x00
-#define ADC_ADON (1 << 0)
-#define ADC_SWSTART (1 << 30)
-#define ADC_EOC (1 << 1)
+#define CR2_ADON (1 << 0)
+#define CR2_SWSTART (1 << 30)
+#define SR_EOC (1 << 1)
 
 void pa1_adc_init(void)
 {
@@ -25,18 +25,18 @@ void pa1_adc_init(void)
     /*Conversion sequence length*/
     ADC1->SQR1 = ADC_SEQ_LEN_1;
     /*Enable ADC module*/
-    ADC1->CR2 |= ADC_ADON;
+    ADC1->CR2 |= CR2_ADON;
 }
 
 void start_conversion(void)
 {
-    ADC1->CR2 |= ADC_SWSTART;
+    ADC1->CR2 |= CR2_SWSTART;
 }
 
 uint32_t adc_read(void)
 {
     /*Wait for conversation to be complete*/
-    while (!(ADC1->SR & ADC_EOC))
+    while (!(ADC1->SR & SR_EOC))
     {
     }
     /*Read converted result*/
